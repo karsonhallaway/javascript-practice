aaaf8a86633519eb92f38555b5aa8b09a8804ad8
karsonhallaway <karsonhallaway@gmail.com>
2025-11-18 20:05:29 -0700

    added practice workshet for loops.

--- app.js
+++ app.js
@@ -1,20 +1,84 @@
-// For loop: calculating sum of even numbers from 1 - 20
-let total = 0
+// Level 1: Ch 1 Print Parade
+// ------------------------
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
 
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-        total += i;
-    }
-}
+// level 1: Ch 2 even enforcer
+// -----------------------
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
-console.log(total);
+// level 1: Ch 3 countdown
+// ---------------------------
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
 
-// Nested loops
-let product = 0
-for (i = 1; i <= 5; i++) {
-    for (j =1; j <= 5; j++) {
-        product = i * j;
-        console.log(`${i} * ${j} = ${product}`);
-    }
-    console.log("-----------------------")
-}
+// level 1: Ch 4 summoner's circle
+// --------------------------------
+// let totalSum = 0
+
+// for (let i = 1; i <= 5; i++) {
+//     totalSum += i;
+// }
+
+// console.log(totalSum);
+
+// level 1: Ch 5 square scorcey
+// -----------------------------
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i * i);
+// }
+
+// Ch6 
+// ------------------------
+//let animals = ["cat", "dog", "rabbit", "parrot"];
+
+//* declare an array of animals
+// loop through each index of the array
+// print each index of the array 
+
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(animals[i]);
+// }
+
+// ch7
+//--------
+// let name = ["Ali", "Sara", "John"];
+// for (let i = 0; i < name.length; i++) {
+//     console.log("Hello", name[i],"!");
+// }
+
+// ch8 
+// declare the original array
+// declare new array
+// loop through the original array 
+//.    on each loop  of the original array multiply each value index by 2 . 
+//          push those values to the new array
+//          print the result. 
+
+// let original = [1, 2, 3, 4,];
+// let doubled = [];
+
+// for (let i = 0; i <= original.length; i++) {
+//     doubled.push(original[i] * 2);
+// }
+
+// console.log(doubled);
+
+// Ch9
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// Ch 10
+// declare an array of numbers from 1 - 10
+// declare an empty arry of odd numbers 
+// loop through each number of the odd array
+// if the number is odd push it to a the new array 
